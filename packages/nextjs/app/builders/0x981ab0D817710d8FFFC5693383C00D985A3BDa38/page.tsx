ceb9bc9e7b78e825a01432122b6ab88f468513d9
zapaz.eth <alain@kredeum.com>
2024-05-13 18:31:05 +0200

    fix window undefined

--- packages/nextjs/app/builders/0x981ab0D817710d8FFFC5693383C00D985A3BDa38/page.tsx
+++ packages/nextjs/app/builders/0x981ab0D817710d8FFFC5693383C00D985A3BDa38/page.tsx
@@ -3,23 +3,24 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import type { NextPage } from "next";
-import { useEnsAvatar, useEnsName } from "wagmi";
+import { useEnsAddress, useEnsAvatar } from "wagmi";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
 
+const ensName = "zapaz.eth";
+
 const Zapaz: NextPage = () => {
-  const address = window.location.pathname.split("/").pop() || "";
-  const [ensName, setEns] = useState<string | null>();
+  const [ensAdddress, setEnsAddress] = useState<string | null>();
   const [ensAvatar, setEnsAvatar] = useState<string | null>();
-  const { data: fetchedEns } = useEnsName({ address, chainId: 1 });
-  const { data: fetchedEnsAvatar } = useEnsAvatar({ name: String(fetchedEns), chainId: 1 });
+  const { data: fetchedEnsAddress } = useEnsAddress({ name: ensName, chainId: 1 });
+  const { data: fetchedEnsAvatar } = useEnsAvatar({ name: ensName, chainId: 1 });
   useEffect(() => setEnsAvatar(fetchedEnsAvatar), [fetchedEnsAvatar]);
-  useEffect(() => setEns(fetchedEns), [fetchedEns]);
+  useEffect(() => setEnsAddress(fetchedEnsAddress), [fetchedEnsAddress]);
 
   return (
     <>
       <div className="m-10 mb-20">
         <div className="bg-base-100 border-base-300 border shadow-md shadow-secondary rounded-3xl px-6 lg:px-8 mb-6 space-y-1 py-4 overflow-x-auto">
-          <BlockieAvatar address={address} ensImage={ensAvatar} size={100} />
+          <BlockieAvatar address={ensAdddress || ""} ensImage={ensAvatar} size={100} />
 
           <div>{ensName}</div>
           <div>
