31ecea5c1d7a9a622353a145ec0e0c2d20791a42
zapaz.eth <alain@kredeum.com>
2024-05-14 18:36:09 +0200

    various fixes

--- packages/nextjs/app/builders/0x981ab0D817710d8FFFC5693383C00D985A3BDa38/page.tsx
+++ packages/nextjs/app/builders/0x981ab0D817710d8FFFC5693383C00D985A3BDa38/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Link from "next/link";
 import type { NextPage } from "next";
 import { useEnsAddress, useEnsAvatar } from "wagmi";
@@ -9,19 +8,21 @@ import { BlockieAvatar } from "~~/components/scaffold-eth";
 const ensName = "zapaz.eth";
 
 const Zapaz: NextPage = () => {
-  const [ensAdddress, setEnsAddress] = useState<string | null>();
-  const [ensAvatar, setEnsAvatar] = useState<string | null>();
-  const { data: fetchedEnsAddress } = useEnsAddress({ name: ensName, chainId: 1 });
-  const { data: fetchedEnsAvatar } = useEnsAvatar({ name: ensName, chainId: 1 });
-  useEffect(() => setEnsAvatar(fetchedEnsAvatar), [fetchedEnsAvatar]);
-  useEffect(() => setEnsAddress(fetchedEnsAddress), [fetchedEnsAddress]);
+  const { data: ensAddress, error: errorAddress } = useEnsAddress({ name: ensName, chainId: 1 });
+  if (errorAddress) console.error("ENS Address Error:", errorAddress);
+
+  const { data: ensAvatar, error: errorAvatar } = useEnsAvatar({ name: ensName, chainId: 1 });
+  if (errorAvatar) console.error("ENS Avator Error:", errorAvatar);
 
   return (
     <>
       <div className="m-10 mb-20">
-        <div className="bg-base-100 border-base-300 border shadow-md shadow-secondary rounded-3xl px-6 lg:px-8 mb-6 space-y-1 py-4 overflow-x-auto">
-          <BlockieAvatar address={ensAdddress || ""} ensImage={ensAvatar} size={100} />
-
+        <div className="bg-base-100 border-base-300 border shadow-md shadow-secondary rounded-3xl px-6 lg:px-8 mb-6 space-y-1  py-4 overflow-x-auto">
+          {ensAddress && ensAvatar ? (
+            <BlockieAvatar address={ensAddress} ensImage={ensAvatar} size={100} />
+          ) : (
+            <span className="loading loading-spinner loading-lg"></span>
+          )}
           <div>{ensName}</div>
           <div>
             Web3 Architect @
@@ -30,16 +31,16 @@ const Zapaz: NextPage = () => {
             </Link>
           </div>
           <div>
-            GitHub repos: &nbsp;
-            <Link href="https://github.com/kredeum/Kredeum" className="link">
+            GitHub repos:
+            <Link href="https://github.com/kredeum/Kredeum" className="link p-2">
               Kredeum NFTs Factory
             </Link>
-            &nbsp; - &nbsp;
-            <Link href="https://github.com/kredeum/opennfts" className="link">
+            -
+            <Link href="https://github.com/kredeum/opennfts" className="link p-2">
               OpenNFTs Library
             </Link>
-            &nbsp; - &nbsp;
-            <Link href="https://github.com/zapaz/forge-deploy-lite" className="link">
+            -
+            <Link href="https://github.com/zapaz/forge-deploy-lite" className="link p-2">
               Forge Deploy Lite
             </Link>
           </div>
