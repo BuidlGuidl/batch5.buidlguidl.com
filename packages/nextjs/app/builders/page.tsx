c8065656b09a11a29a4ba7c91159c74b016e4899
zapaz.eth <alain@kredeum.com>
2024-05-19 19:18:43 +0200

    simplify list of Builder with daisy UI and SE Address component

--- packages/nextjs/app/builders/page.tsx
+++ packages/nextjs/app/builders/page.tsx
@@ -1,45 +1,25 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import Link from "next/link";
 import type { NextPage } from "next";
 import type { Address as AddressType } from "viem";
-import { useEnsAvatar, useEnsName } from "wagmi";
-import { BlockieAvatar } from "~~/components/scaffold-eth";
+import { Address } from "~~/components/scaffold-eth";
 import { useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
 
 const Builder = ({ address }: { address: AddressType }) => {
-  const [ensName, setEns] = useState<string | null>();
-  const [ensAvatar, setEnsAvatar] = useState<string | null>();
-
-  const { data: fetchedEns, error: errorName, isFetched: isFetchedEns } = useEnsName({ address, chainId: 1 });
-  if (errorName) console.error("ENS Name Error:", errorName, "for", address);
-
-  const {
-    data: fetchedEnsAvatar,
-    error: errorAvatar,
-    isFetched: isFetchedAvatar,
-  } = useEnsAvatar({ name: String(fetchedEns), chainId: 1 });
-  if (errorAvatar) console.error("ENS Avator Error:", errorAvatar, "for", address);
-
-  useEffect(() => setEnsAvatar(fetchedEnsAvatar), [fetchedEnsAvatar]);
-  useEffect(() => setEns(fetchedEns), [fetchedEns]);
-
   return (
     <>
-      <div className="m-10 ">
-        <div className="bg-base-100 border-base-300 border shadow-md shadow-secondary rounded-3xl px-6 lg:px-8 mb-6 space-y-1 py-4 overflow-x-auto">
-          <Link href={`/builders/${address}`}>
-            {isFetchedAvatar ? (
-              <BlockieAvatar address={address} ensImage={ensAvatar} size={100} />
-            ) : (
-              <span className="loading loading-spinner loading-lg"></span>
-            )}
-            <div>{isFetchedEns ? ensName : "*****"}</div>
-            <div>{address}</div>
-          </Link>
+      <li>
+        <hr />
+        <div className="timeline-start timeline-box">
+          <Address address={address} />
         </div>
-      </div>
+        <div className="timeline-middle">
+          <a href={`/builders/${address}`}>
+            <span style={{ fontSize: "22px" }}>👀</span>
+          </a>
+        </div>
+        <hr />
+      </li>
     </>
   );
 };
@@ -53,11 +33,21 @@ const Builders: NextPage = () => {
 
   return (
     <>
-      {data ? (
-        data.map((log, index) => <Builder key={index} address={log.args.builder || ""} />)
-      ) : (
-        <span className="loading loading-spinner loading-lg"></span>
-      )}
+      <ul className="timeline timeline-vertical">
+        <li>
+          <div className="timeline-middle timeline-box">List of Batch #5 Builders</div>
+          <hr />
+        </li>
+        {data && data.length > 0 ? (
+          data.map((log, index) => <Builder key={index} address={log.args.builder || ""} />)
+        ) : (
+          <li>
+            <div className="timeline-middle timeline-box">
+              <span className="loading loading-spinner loading-lg"></span>
+            </div>
+          </li>
+        )}
+      </ul>
     </>
   );
 };
