4083c6df8941f871b013832c8c66fccdbf586531
i-ex3c <ekoduemmanuelaudu@gmail.com>
2024-05-23 00:33:29 +0100

    ...

--- packages/nextjs/app/builders/0xbFbccAd69D2B42339674F4988Bf5f200fBDe3004/page.tsx
+++ packages/nextjs/app/builders/0xbFbccAd69D2B42339674F4988Bf5f200fBDe3004/page.tsx
@@ -3,47 +3,48 @@ import type { NextPage } from "next";
 import { AcademicCapIcon, BriefcaseIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { MapPinIcon } from "@heroicons/react/24/solid";
 
-const Profile: NextPage = () => {
-  return (
-    <>
-      <div className="flex items-center flex-col flex-grow pt-10 justify-center">
-        <div className="h-1/2 w-2/4 flex flex-col justify-center items-center bg-secondary p-6 rounded-2xl">
-          <div className="flex gap-10">
-            <div className="">
-              <Image
-                src="/assets/chris.png"
-                className="rounded-full"
-                alt="Ezekiel Christian Emmanuelaudu"
-                width={200}
-                height={200}
-              />
+const Profilepage: NextPage = () => {
+    return(
+        <>
+        <div className="w-full h-screen bg-center flex items-center justify-center" >
+            <div className="text-center p-96 text-white relative rounded-2xl">
+                <Image
+                    src="/assets/chris.png"
+                    className="rounded-full"
+                    alt="Chris"
+                    width={200}
+                    height={200}
+                 />
+                <h3>Ezekiel Christian Emmanuelaudu</h3>
+                <p>Pure Designer | Growing Developer</p>>
+                <p style="font-size: 15px; padding: 3px; margin-top: 2px;">
+                Address: 0xbFbccAd69D2B42339674F4988Bf5f200fBDe3004</p>
+                <p></p>
+                <div class="social-media">
+
+                    <a href="https://www.instagram.com/ex3cofficial">
+                        <Image src="/assets/instagram.png" alt="Instagram" width={25} height={25} /> </a>
+                    <a href="https://x.com/ekondu_e">
+                        <Image src="/assets/x.png" alt="X" width={25} height={25} /></a>
+                    <a href="https://t.me/i_ex3c">
+                        <Image src="/assets/telegram.png" alt="Telegram" width={25} height={25} /></a>
+                </div>
+
+                <a href="mailto:ekonduemmanuel@gmail.com?subject=A%20mail%20from%20your%20buidlguidl%profile%page">
+                    Say hello
+                </a>
+
+              
+                <h2 className="text-zinc-800 p-96 -mr-24 -ml-24 -mb-10 rounded-2xl">
+                My name is Ezekiel Ekondu Christian Emmanuelaudu, &apos;
+                I'm a Product Designer, 3d modelling artist, Motion &apos; graphics designer and growing Developer.
+                </h2>
+
             </div>
-            <div className="my-auto flex flex-col gap-1">
-              <h1 className="text-3xl">Ezekiel Christian Emmanuelaudu</h1>
-              <div className="flex gap-2">
-                <AcademicCapIcon className="h-5" />
-                <h1>Pure Designer | Growing Developer</h1>
-                <h1> Address: 0xbFbccAd69D2B42339674F4988Bf5f200fBDe3004</h1>
-              </div>
-        
-              <a href="https://twitter.com/ekondu_e" target="_blank" className="flex gap-2">
-                <XMarkIcon className="h-5" />
-                <div className="">X.com</div>
-              </a>
-        
-            </div>
-          </div>
-          <div className="flex justify-center">
-            <h2 className="text-center text-xl mt-4 max-w-screen-lg leading-6">
-            My name is Ezekiel Ekondu Christian Emmanuelaudu, &apos;
-                I'm a Product Designer, 3d modelling artist, Motion &apos;
-                graphics designer and growing Developer.
-            </h2>
-          </div>
+
         </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
-export default Profile;
+export default Profilepage;
