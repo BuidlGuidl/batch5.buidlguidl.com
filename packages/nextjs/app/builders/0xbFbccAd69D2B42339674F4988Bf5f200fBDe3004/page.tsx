6a2a4c1337d88f5387a888df8f5c4560ce54d3ea
i-ex3c <ekoduemmanuelaudu@gmail.com>
2024-05-22 23:27:03 +0100

    newwer

--- packages/nextjs/app/builders/0xbFbccAd69D2B42339674F4988Bf5f200fBDe3004/page.tsx
+++ packages/nextjs/app/builders/0xbFbccAd69D2B42339674F4988Bf5f200fBDe3004/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import type { NextPage } from "next";
-import { AcademicCapIcon, BriefcaseIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { MapPinIcon } from "@heroicons/react/24/solid";
 
 const Profilepage: NextPage = () => {
     return(
